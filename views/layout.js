46dd8b015b8c732b304a09bca1ca70b2293f901e
Nathan-Payne <paynee89@yahoo.co.uk>
2020-07-06 17:01:06 +0100

    apply redesign to layout html, reset css

--- views/layout.js
+++ views/layout.js
@@ -1,64 +1,83 @@
 module.exports = ({ content }) => {
 	return `
     <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>PRNT Store</title>
-        
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css"></link>
-        <link href="/css/main.css" rel="stylesheet">
-      </head>
+<html lang="en">
 
-      <body>
-        <header>
-          <nav class="navbar navbar-top">
-            <div class="container navbar-container">
-              <div>
-                <ul class="social">
-                  <li>
-                    <a href=""><i class="fa fa-phone"></i>+1 555 987 6543</a>
-                  </li>
-                  <li>
-                    <a href=""><i class="fa fa-envelope"></i> shop@myshop.com</a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <ul class="social">
-                  <li><a href=""><i class="fab fa-facebook"></i></a></li>
-                  <li><a href=""><i class="fab fa-twitter"></i></a></li>
-                  <li><a href=""><i class="fab fa-linkedin"></i></a></li>
-                  <li><a href=""><i class="fab fa-instagram"></i></a></li>
-                </ul>
-              </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PRNT Store</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+
+            <div class="ham-container">
+                <input type="checkbox" id="nav-toggle">
+                <label for="nav-toggle" class="hamburger-menu">
+                    <div class="menu-lines">
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                    </div>
+                </label>
             </div>
-          </nav>
-          <nav class="navbar navbar-bottom">
-            <div class="container navbar-container">
-              <div>
+
+            <div class="logo-container">
                 <a href="/">
-                  <h3 class="title">EComm Shop</h3>
+                    <h2 class="logo">PRNT</h2>
                 </a>
-              </div>
-              <div class="navbar-item">
-                <div class="navbar-buttons">
-                  <div class="navbar-item">
-                    <a href="/"><i class="fa fa-star"></i> Products</a>
-                  </div>
-                  <div class="navbar-item">
-                    <a href="/cart"><i class="fa fa-shopping-cart"></i> Cart</a>
-                  </div>
-                </div>
-              </div>
             </div>
-          </nav>
-        </header>
 
+            <div class="cart-icon">
+                <a href="/cart"><i class="fa fa-shopping-cart"></i></a>
+            </div>
+
+        </nav>
+    </header>
+
+    <hr>
+
+    <section>
         ${content}
-      </body>
-    </html>
+    </section>
+
+    <hr>
+
+    <section id="about">
+        <h1>WHO WE ARE</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus netus auctor tempus etiam purus bibendum mauris quam.</p>
+        <button class="btn-outline-tr">JOIN THE MAILING LIST</button>
+    </section>
+
+    <hr>
+
+    <footer id="contact">
+        <div>
+            <ul class="contact-info">
+                <li>
+                    <a href=""><i class="fa fa-phone"></i>+1 555 987 6543</a>
+                </li>
+                <li>
+                    <a href=""><i class="fa fa-envelope"></i> shop@myshop.com</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <ul class="social">
+                <li><a href=""><i class="fab fa-facebook"></i></a></li>
+                <li><a href=""><i class="fab fa-twitter"></i></a></li>
+                <li><a href=""><i class="fab fa-linkedin"></i></a></li>
+                <li><a href=""><i class="fab fa-instagram"></i></a></li>
+            </ul>
+        </div>
+    </footer>
+    <hr>
+</body>
+
+</html>
   `;
 };
